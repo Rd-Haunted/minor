f6e24c932a409a9d2a8170a32f243a4a43dd486f
Rd_Haunted <rohitdey1204@gmail.com>
2024-11-25 23:13:04 -0500

    delete responsive file

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ let menu = document.querySelector('.toggle_btn');
 let navlist = document.querySelector('.headerinner_right_bottom_div');
 
 menu.onclick = () => {
-    if(menu = 1){
-        navlist.style.right=0;
-    }else if (menu = 0){
-        navlist.style.right=`${-100}%`;
-    }
-    
+    navlist.classList.toggle('Active');
 }
 
